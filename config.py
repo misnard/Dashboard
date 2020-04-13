006d3f6992d4d51adbf09b586bc90fc7b2504d89
yunows <maher.isnard@gmail.com>
2020-04-13 13:52:27 +0200

    Configure production requirements

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
 
 
-# To generate a new secret key:
-# >>> import random, string
-# >>> "".join([random.choice(string.printable) for _ in range(24)])
-SECRET_KEY = "&~x0br:J=nTMPxio#A67 V`7i_1"
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
